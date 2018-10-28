46bc4caa9abceb9e88cdb28903c91f4344850506
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-10-28 17:00:21 +0300

    style: fix code style to correspond to Airbnb style guide

--- sumOfOther.js
+++ sumOfOther.js
@@ -1,4 +1,4 @@
 module.exports = function sumOfOther(arr) {
-    let sum = arr.reduce((a, b) => a + b, 0);
-    return arr.map(elem => sum - elem);
-}
+  const sum = arr.reduce((a, b) => a + b, 0);
+  return arr.map(elem => sum - elem);
+};
--- test.js
+++ test.js
@@ -1,89 +1,93 @@
-let assert = require('assert');
-let sumOfOther = require('./sumOfOther.js');
-let make = require('./make.js');
-let recursion = require('./recursion.js')
-describe("Each element is the sum of other elements", function() {
-    it("Positive integer values", function() {
-        assert.deepStrictEqual(sumOfOther([2, 3, 4, 1]), [8, 7, 6, 9]);
-    });
-    it("Zero values", function() {
-        assert.deepStrictEqual(sumOfOther([0, 0, 0, 0]), [0, 0, 0, 0]);
-    });
-    it("Negative and positive values", function() {
-        assert.deepStrictEqual(sumOfOther([-6, 4, 0, 1000]), [1004, 994, 998, -2]);
-    });
-    it("Single value", function() {
-        assert.deepStrictEqual(sumOfOther([3]), [0]);
-    });
-    it("Empty array", function() {
-        assert.deepStrictEqual(sumOfOther([]), []);
-    });
+const assert = require('assert');
+const sumOfOther = require('./sumOfOther.js');
+const make = require('./make.js');
+const recursion = require('./recursion.js');
+
+describe('Each element is the sum of other elements', () => {
+  it('Positive integer values', () => {
+    assert.deepStrictEqual(sumOfOther([2, 3, 4, 1]), [8, 7, 6, 9]);
+  });
+  it('Zero values', () => {
+    assert.deepStrictEqual(sumOfOther([0, 0, 0, 0]), [0, 0, 0, 0]);
+  });
+  it('Negative and positive values', () => {
+    assert.deepStrictEqual(sumOfOther([-6, 4, 0, 1000]), [1004, 994, 998, -2]);
+  });
+  it('Single value', () => {
+    assert.deepStrictEqual(sumOfOther([3]), [0]);
+  });
+  it('Empty array', () => {
+    assert.deepStrictEqual(sumOfOther([]), []);
+  });
 });
 
-describe("Apply function to arguments", function() {
-    it("Sum numbers", function() {
-        assert.equal(make(15)(34, 21, 666)(41)((a, b) => a + b), 777);
-    });
-    it("Multiply numbers", function() {
-        assert.equal(make(45)(12, 0)(2)((a, b) => a * b), 0); 
-    });
-    it("Empty argument", function() {
-        assert.equal(make()((a, b) => a / b), 0); 
-    });
+describe('Apply function to arguments', () => {
+  it('Sum numbers', () => {
+    assert.equal(make(15)(34, 21, 666)(41)((a, b) => a + b), 777);
+  });
+  it('Multiply numbers', () => {
+    assert.equal(make(45)(12, 0)(2)((a, b) => a * b), 0);
+  });
+  it('Empty argument', () => {
+    assert.equal(make()((a, b) => a / b), 0);
+  });
 });
 
-describe("Recursion", function() {
-    it("Sample test", function() {
-        assert.deepStrictEqual(
-            recursion({
-                value: 100,
-                left: {
-                    value: 90,
-                    left: {
-                        value: 70
-                    },
-                    right: {
-                        value: 99
-                    }
-                },
-                right: {
-                    value: 120,
-                    left: {
-                        value: 110
-                    },
-                    right: {
-                        value: 130
-                    }
-                }
-            }), [
-                [100],
-                [90, 120],
-                [70, 99, 110, 130]
-            ]);
-    });
-    it("Tree with only left elements", function() {
-        assert.deepStrictEqual(
-            recursion({
-                value: 100,
-                left: {
-                    value: 90,
-                    left: {
-                        value: 70,
-                        left: {
-                            value: 50
-                        }
-                    }
-                },
-            }), [
-                [100],
-                [90],
-                [70],
-                [50]
-            ]);
-    });
-    it("Tree is empty", function() {
-        assert.deepStrictEqual(
-            recursion({}), 
-            []);
-    });
+describe('Recursion', () => {
+  it('Sample test', () => {
+    assert.deepStrictEqual(
+      recursion({
+        value: 100,
+        left: {
+          value: 90,
+          left: {
+            value: 70,
+          },
+          right: {
+            value: 99,
+          },
+        },
+        right: {
+          value: 120,
+          left: {
+            value: 110,
+          },
+          right: {
+            value: 130,
+          },
+        },
+      }), [
+        [100],
+        [90, 120],
+        [70, 99, 110, 130],
+      ],
+    );
+  });
+  it('Tree with only left elements', () => {
+    assert.deepStrictEqual(
+      recursion({
+        value: 100,
+        left: {
+          value: 90,
+          left: {
+            value: 70,
+            left: {
+              value: 50,
+            },
+          },
+        },
+      }), [
+        [100],
+        [90],
+        [70],
+        [50],
+      ],
+    );
+  });
+  it('Tree is empty', () => {
+    assert.deepStrictEqual(
+      recursion({}),
+      [],
+    );
+  });
 });
