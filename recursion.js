1cc03659908422c6112528159edd0745d50d841d
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-10-28 17:03:17 +0300

    refactor: implement IIFE to fix Airbnb eslint error

--- recursion.js
+++ recursion.js
@@ -1,15 +1,22 @@
-module.exports = function recursion(tree, arr = [], level = 0) {
-    if (!tree || Object.keys(tree).length === 0) {
-        return arr;
+module.exports = function recursion(tree) {
+  const arr = [];
+
+  (function realRecursion(node, level) {
+    if (!node || Object.keys(node).length === 0) {
+      return arr;
     }
 
     if (typeof arr[level] === 'undefined') {
-        arr[level] = [ tree.value ];
+      arr[level] = [node.value];
     } else {
-        arr[level].push(tree.value);
+      arr[level].push(node.value);
     }
 
-    recursion(tree.left, arr, level + 1);
-    recursion(tree.right, arr, level + 1);
-    return arr;
-}
+    realRecursion(node.left, level + 1);
+    realRecursion(node.right, level + 1);
+
+    return node.value;
+  }(tree, 0));
+
+  return arr;
+};
